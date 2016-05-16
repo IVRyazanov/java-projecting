e8ee4dde65e5f6e0485d1b2c29286fa53b844281
unknown <Ivan.Ryazanov@RYAZANOV-PC.IT-NS.local>
2016-05-16 13:31:20 +0300

    Запуск БД HSQLDB через плагин мавена exec:java даллее запуск EventManager с параметром store

--- src/main/java/hibernate/HibernateUtil.java
+++ src/main/java/hibernate/HibernateUtil.java
@@ -11,21 +11,25 @@ import java.nio.file.Paths;
  * Created by Иван on 12.05.2016.
  */
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory = createFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+/*
+mvn exec:java -Dexec.mainClass="org.hsqldb.Server" -Dexec.args="-database.0 file:target/data/tutorial"
+ */
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
-
-    private static SessionFactory createFactory() {
-        try {
-            return new Configuration().configure().buildSessionFactory();
-        } catch (Throwable throwable) {
-            System.err.println("Unable to create session");
-            throw new ExceptionInInitializerError(throwable);
-        }
-    }
-
 
 }
 
