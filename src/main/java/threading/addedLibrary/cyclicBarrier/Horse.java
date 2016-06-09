eaf03d143490b4f42b97747d74e1c664faf17760
unknown <Ivan.Ryazanov@RYAZANOV-PC.IT-NS.local>
2016-06-09 11:26:08 +0300

    Delay очередь из потоков , которые выстраиваются по времени

--- src/main/java/threading/addedLibrary/cyclicBarrier/Horse.java
+++ src/main/java/threading/addedLibrary/cyclicBarrier/Horse.java
@@ -1,6 +1,7 @@
 package threading.addedLibrary.cyclicBarrier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
